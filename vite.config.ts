0016d958e0c0ac2d70271af0e6d6f74d5131661d
samemery <s.emery@reckondigital.com>
2025-08-27 16:45:10 +0100

    updated to get sandbox working

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  server: { host: true, port: 5173 },
   plugins: [
     react(),
     runtimeErrorOverlay(),
